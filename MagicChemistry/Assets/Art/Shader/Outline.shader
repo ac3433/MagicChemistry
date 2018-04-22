7813456ba81efd57bdb9d4e05cd1d5be299d09e7
Alexander <ac3433@drexel.edu>
2018-04-22 13:37:06 -0400

    Added  - Grid generator  - Temp scene to work on grid generator
    Modify
     - Tube data to hold points
     - Tube operation data calculation method

---
+++ MagicChemistry/Assets/Art/Shader/Outline.shader
@@ -0,0 +1,83 @@
+﻿Shader "Unlit/Outline"
+{
+	Properties
+	{
+		_Color("Main Color", Color) = (0.5, 0.5,0.5)
+		_MainTex ("Texture", 2D) = "white" {}
+		_OutlineColor("Outline Color", Color) = (0,0,0,1)
+		_OutlineWidth("Outline Width", Range(1.0,5.0)) = 1.01 
+	}
+
+	CGINCLUDE	
+// Upgrade NOTE: excluded shader from DX11; has structs without semantics (struct v2f members POSITION)
+#pragma exclude_renderers d3d11
+	#include "UnityCG.cginc"
+
+	struct appdata{
+		float4 vertex : POSITION;
+		float3 normal : NORMAL;
+	};
+
+	struct v2f
+	{
+		float4 POSITION;
+		float4 color : COLOR;
+		float3 normal : NROMAL;
+	};
+
+	float _OutlineWidth;
+	float4 _OutlineColor;
+
+	v2f vert(appdata v)
+	{
+		v.vertex.xyz *= _OutlineWidth;
+
+		v3f o;
+		o.pos = UnityObjectToClipPos(v.vertex);
+		o.color = _OutlineColor;
+		return o;
+	}
+
+	ENDCG
+
+	SubShader
+	{
+		Pass
+		{
+			ZWrite Off
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			half4 frag(v2f i) : Color
+			{
+				return i.color
+			}
+			ENDCG
+		}
+
+		Pass
+		{
+			ZWrite On
+
+			Material
+			{
+				Diffuse[_Color]
+				Ambient[_Color]
+			}
+
+			Lighting On
+
+			SetTexture[_MainTex]
+			{
+				ConstantColor[_Color]
+			}
+
+			SetTexture[_MainTex]
+			{
+				Combine previous * primary DOUBLE
+			}
+		}
+	}
+}
---
+++ MagicChemistry/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct GridPlacement
+{
+    public int xCoordinate;
+    public int yCoordinate;
+    [Tooltip("Select the sprite from the tile list")]
+    public int spriteID;
+}
+
+public class LevelManager : MonoBehaviour {
+    
+    [SerializeField]
+    private GameObject[] _tile;
+
+    [SerializeField]
+    private GameObject _startPosition;
+
+    [SerializeField]
+    private List<GridPlacement> _specialTile;
+
+    [SerializeField]
+    [Tooltip("It will create a perfect sqaure grid based on the number. Ex. 8 will create 8x8 grid.")]
+    [Range(2,8)]
+    private byte _gridSize = 2;
+
+    private GameObject[,] _grid;
+
+	void Start () {
+		if(_tile == null)
+        {
+            Debug.LogError(string.Format("GameObject: {0}\nScript: LevelManager\nError: Missing tile game object.",gameObject.name));
+            return;
+        }
+
+        if (_startPosition == null)
+        {
+            Debug.LogError(string.Format("GameObject: {0}\nScript: LevelManager\nError: Missing start position game object.", gameObject.name));
+            return;
+        }
+
+        CreateInteractableTile();
+	}
+
+    public float TileSize() { return _tile[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+	
+    private void CreateInteractableTile()
+    {
+        _grid = new GameObject[_gridSize, _gridSize];
+
+        foreach (GridPlacement placement in _specialTile)
+        {
+            PlaceTile(placement.xCoordinate, placement.yCoordinate, placement.spriteID);
+        }
+
+        for (int y = 0; y < _gridSize; y++)
+        {
+            for(int x = 0; x < _gridSize; x++)
+            {
+                if(_grid[x,y] == null)
+                    PlaceTile(x, y, 0);
+            }
+        }
+
+    }
+
+    public void PlaceTile(int x, int y, int tileID)
+    {
+        if(x < 0 || x > _gridSize)
+        {
+            Debug.LogError(string.Format("GameObject: {0}\nScript: LevelManager\nError: X coordinate is out of bound.",gameObject.name));
+            return;
+        }
+
+        if (y < 0 || y > _gridSize)
+        {
+            Debug.LogError(string.Format("GameObject: {0}\nScript: LevelManager\nError: Y coordinate is out of bound.",gameObject.name));
+            return;
+        }
+
+        if (tileID < 0 || tileID >_tile.Length)
+        {
+            Debug.LogError(string.Format("GameObject: {0}\nScript: LevelManager\nError: tile id is out of bound.",gameObject.name));
+            return;
+        }
+
+        GameObject newTile = Instantiate(_tile[tileID]);
+        newTile.transform.parent = _startPosition.transform;
+        newTile.transform.position = new Vector3( _startPosition.transform.position.x + (TileSize() * x), _startPosition.transform.position.y - (TileSize() * y));
+        newTile.name = string.Format("Tile {0}x{1}", x, y);
+        _grid[x,y] = newTile;
+    }
+
+}
