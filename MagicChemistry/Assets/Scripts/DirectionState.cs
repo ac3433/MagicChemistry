5dbf210964d3f5e6f7a68a3aac816ab4aa7181ec
Alexander <ac3433@drexel.edu>
2018-05-02 21:20:45 -0400

    adding singleton to levelmanager

--- MagicChemistry/Assets/Scripts/DirectionState.cs
+++ MagicChemistry/Assets/Scripts/DirectionState.cs
@@ -5,3 +5,46 @@ public enum DirectionState
 {
     North, West, South, East
 }
+
+/// <summary>
+/// Utility class adding more functionality to the Direction
+/// Usage: Call this specific class name to access the function.
+/// Note: Couldn't extend the proper functionality to the enum itself.
+/// </summary>
+public static class DirectionExtension
+{
+
+    public static int GetDirectionPointX(DirectionState dir)
+    {
+        switch (dir)
+        {
+            case DirectionState.North:
+                return 0;
+            case DirectionState.East:
+                return 1;
+            case DirectionState.West:
+                return -1;
+            case DirectionState.South:
+                return 0;
+            default:
+                return 0;
+        }
+    }
+
+    public static int GetDirectionPointY(DirectionState dir)
+    {
+        switch (dir)
+        {
+            case DirectionState.North:
+                return 1;
+            case DirectionState.East:
+                return 0;
+            case DirectionState.West:
+                return 0;
+            case DirectionState.South:
+                return -1;
+            default:
+                return 0;
+        }
+    }
+}
